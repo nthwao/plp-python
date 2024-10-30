9a4d561aca37211b0b96037e40caa5453613df51
michael nthwao <134537722+nthwao@users.noreply.github.com>
2024-10-30 20:12:42 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+
+# Collect user name, age, and location
+name = input("Hello, What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+
+# Printm
+#  message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
